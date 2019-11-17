bcdd159de429e03cb9f0f651cd8c0a0261b91a80
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-16 16:21:14 -0800

    Add ESlint, experiment with lists and flexbox stylings

--- CheckerTexter/App.js
+++ CheckerTexter/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import StudentList from './src/StudentList.js';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Checker Texter Grades</Text>
+      <StudentList />
     </View>
   );
 }
@@ -12,6 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: Constants.statusBarHeight,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
