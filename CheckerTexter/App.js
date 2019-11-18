56bd8e81ad7442c43845aca31065d14fb1dffaea
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-17 17:23:32 -0800

    Remove excess comments and move to hardcoded data

--- CheckerTexter/App.js
+++ CheckerTexter/App.js
@@ -2,66 +2,40 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import StudentList from './src/StudentList.js';
-import sampleStudents from '/Users/lilcare/projects/MVP/checker-texter/CheckerTexter/sampleData/sampleStudentData.js';
-// import {SAMPLEDATA, sampleStudents} from './sampleData/sampleStudentData.js';
 
-// const DATA = SAMPLEDATA;
-const students = sampleStudents;
-
-const DATA = [
+const sampleStudents = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Student 1',
+    firstName: 'Student1',
+    lastName: 'One',
+    phoneNumber: '+14025987648'
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Student 2',
+    firstName: 'Student2',
+    lastName: 'Two',
+    phoneNumber: '+14025987648'
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Student 3',
+    firstName: 'Student3',
+    lastName: 'Three',
+    phoneNumber: '+14025987648'
   },
 ];
-
-// const students = [
-//   {
-//     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-//     firstName: 'Student1',
-//     lastName: 'One',
-//     phoneNumber: '+14025987648'
-//   },
-//   {
-//     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-//     firstName: 'Student2',
-//     lastName: 'Two',
-//     phoneNumber: '+14025987648'
-//   },
-//   {
-//     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-//     firstName: 'Student3',
-//     lastName: 'Three',
-//     phoneNumber: '+14025987648'
-//   },
-// ];
 
 export default class App extends Component {
   state = { students: [] };
 
   componentDidMount() {
-    this.setState({ students: students })
+    this.setState({ students: sampleStudents })
   }
 
   render() {
-    let studentListReadyToRender = (this.state.students.length > 0) ? true : false;
-
     return (
       <View style={styles.container}>
         <Text>Checker Texter Grades</Text>
-        {studentListReadyToRender ? (
-          <StudentList students={this.state.students}/>
-        ): (
-          <StudentList students={DATA}/>
-        )}
+        <StudentList students={this.state.students}/>
       </View>
     );
   }
--- CheckerTexter/sampleData/sampleStudentData.js
+++ CheckerTexter/sampleData/sampleStudentData.js
@@ -18,3 +18,19 @@ const students = [
       phoneNumber: '+14025987648'
     },
   ];
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Student 1',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Student 2',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Student 3',
+  },
+];
+
