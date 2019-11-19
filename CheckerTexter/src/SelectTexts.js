08750539b5a7a1fb9251418a608c1fdf1144fb3c
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-18 22:28:03 -0800

    Implement state changes for button presses

--- CheckerTexter/src/SelectTexts.js
+++ CheckerTexter/src/SelectTexts.js
@@ -6,13 +6,13 @@ const Separator = () => {
 }
 
 const Select = ({selection, callback}) => {
-    console.log(selection)
     return (
         <View style={[styles.button, {backgroundColor: selection.backgroundColor[0]}]}>
             <Button
                 title={selection.text[0]}
                 color={selection.textColor[0]}
                 onPress={() => callback()}
+                style={{letterSpacing: 1.5}}
             />
         </View>
     );
@@ -22,38 +22,62 @@ class SelectTexts extends Component {
     state = {
         complete: {
             text: ["Select students with 'complete'", "Students with 'complete' selected"],
-            backgroundColor: ['#99b5c9'],
-            textColor: ['black'],
+            backgroundColor: ['#D9E4EB', '#99b5c9'],
+            textColor: ['#5F6060', 'black'],
+            selected: false,
         },
         semiComplete: {
             text: ["Select students with 'semi-complete'",  "Students with 'semi-complete' selected"],
-            backgroundColor: ['#E8AB5C'],
-            textColor: ['black'],
+            backgroundColor: ['#F6E0C3', '#E8AB5C'],
+            textColor: ['#5F6060', 'black'],
+            selected: false,
         },
         incomplete: {
             text: ["Select students with 'incomplete'",  "Students with 'incomplete' selected"],
-            backgroundColor: ['#AFBAB5'],
-            textColor: ['black'],
+            backgroundColor: ['#E0C4C3', '#AA5D5C'],
+            textColor: ['#5F6060', 'black'],
+            selected: false,
         },
+    }
+
+    select(value) {
+        let state = this.state[value];
+        
+        // Pop off each value to shift to front as new styling
+        const newText = state.text.pop();
+        const newBackgroundColor = state.backgroundColor.pop();
+        const newTextColor = state.textColor.pop();
+
+        // Shift each value to the front and switch selected to opposite
+        state.text.unshift(newText);
+        state.backgroundColor.unshift(newBackgroundColor);
+        state.textColor.unshift(newTextColor);
+        state.selected = !state.selected;
+
+        // Store state for passed value as newState and set newState
+        const newState = {};
+        newState[value] = state;
+        this.setState(newState);
     }
     
     render() { 
         return (
           <View style={styles.container} >
-              
+
             <Separator style={styles.separator} />
             
-            <Select selection={this.state.complete} callback={() => Alert.alert('Simple Button pressed')}/>
-            <Select selection={this.state.semiComplete} callback={() => Alert.alert('Simple Button pressed')}/>
-            <Select selection={this.state.incomplete} callback={() => Alert.alert('Simple Button pressed')}/>
+            <Select selection={this.state.complete} callback={() => this.select('complete')}/>
+            <Select selection={this.state.semiComplete} callback={() => this.select('semiComplete')}/>
+            <Select selection={this.state.incomplete} callback={() => this.select('incomplete')}/>
             
             <Separator style={styles.separator} />
 
-            <View>
+            <View style={styles.text}>
               <Button
-                title="Press me"
-                disabled
+                title="Save Scores and Text"
+                color='#afbab5'
                 onPress={() => Alert.alert('Cannot press this one')}
+                style={{ letterSpacing: 2 }}
               />
             </View>
             
@@ -65,17 +89,26 @@ class SelectTexts extends Component {
   const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginVertical: 8,
+        //marginVertical: 8,
+        marginBottom: 22
     },
     separator: {
-        marginVertical: 8,
+        marginTop: 8,
+        marginBottom: 16,
         borderBottomColor: '#737373',
         borderBottomWidth: StyleSheet.hairlineWidth,
     },
     button: {
         paddingVertical: 4,
+        marginHorizontal: 12,
         marginBottom: 8,
-    }
+    },
+    text: {
+        padding: 8,
+        fontSize: 20,
+        alignContent: 'center',
+        backgroundColor: '#182f40',   
+      },
   });
   
 export default SelectTexts;
