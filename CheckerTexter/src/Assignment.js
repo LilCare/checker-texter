f6f8e4d1a1bc031ff4a41082d50ec14e39f2af09
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-25 13:59:51 -0800

    Fix stylings for the assignment modal

--- CheckerTexter/src/Assignment.js
+++ CheckerTexter/src/Assignment.js
@@ -14,9 +14,9 @@ function Assignment ({setModalInvisible, setTitle, setDate, saveAssignment, visi
         <View style={{ marginTop: 22 }}>
           <View style={styles.modal}>
 
-            <Text>Enter information about your assignment.</Text>
+            <Text style={styles.directions}>Enter the title and date for the assignment</Text>
             
-            <Text style={[styles.font, styles.label]}>Title:</Text>
+            {/* <Text style={[styles.font, styles.label]}>Title:</Text> */}
             <TextInput
               style={[styles.font, styles.titleInput]}
               placeholder="Assignment Title"
@@ -24,7 +24,7 @@ function Assignment ({setModalInvisible, setTitle, setDate, saveAssignment, visi
               value={assignment}
             />
 
-            <Text style={[styles.font, styles.label]}>Date:</Text>
+            {/* <Text style={[styles.font, styles.label]}>Date:</Text> */}
             <DatePickerIOS
               style={[styles.font, styles.dateInput]}
               mode="date"
@@ -47,59 +47,63 @@ function Assignment ({setModalInvisible, setTitle, setDate, saveAssignment, visi
 }
 
 const styles = StyleSheet.create({
-    modal: {
-        marginTop: 20,
-        height: 800,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#afbab5', 
-        backgroundColor: 'white', 
-        alignContent:'stretch',
-        // color: '#afbab5',
-    },
-    font: {
-      fontSize: 18,
-      letterSpacing: 1.5,
-      padding: 8,
-    },
-    label: {
-      alignContent: 'flex-start',
-    },
-    select: {
-        fontSize: 18,
-        padding: 16,
-        letterSpacing: 1.5,
-        color: '#182f40',
-        color: '#afbab5',
-        fontWeight: 'bold',
-    },
-    titleInput: {
-      padding: 10,
-      borderColor: 'gray',
-      borderWidth: 1,
-      borderRadius: 2,
-      backgroundColor: 'white',
-      width: 350,
-      marginBottom: 16,
-    },
-    dateInput: {
-      backgroundColor: 'white',
-      width: 350,
-      marginBottom: 16,
-    },
-    createAssignment: {
-        marginTop: 8,
-        alignContent: 'center',
-        backgroundColor: '#182f40',  
-        borderRadius: 5,
-    },
+  modal: {
+    marginTop: 20,
+    height: 800,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#afbab5', 
+    backgroundColor: 'white', 
+    alignContent:'stretch',
+    
+    // color: '#afbab5',
+  },
+  directions: {
+    color: '#182f40',
+    fontSize: 26,
+    letterSpacing: 1.5,
+    textAlign: 'center',
+    marginBottom: 60,
+    width: 300
+  },
+  font: {
+    color: '#182f40',
+    fontSize: 18,
+    letterSpacing: 1.5,
+    padding: 8,
+  },
+  label: {
+    alignContent: 'flex-start',
+  },
+  select: {
+    fontSize: 18,
+    padding: 16,
+    letterSpacing: 1.5,
+    color: '#182f40',
+    color: '#afbab5',
+    fontWeight: 'bold',
+  },
+  titleInput: {
+    padding: 10,
+    borderColor: '#182f40',
+    borderWidth: 1,
+    borderRadius: 2,
+    backgroundColor: 'white',
+    width: 300,
+    marginBottom: 16,
+  },
+  dateInput: {
+    backgroundColor: 'white',
+    width: 300,
+    marginBottom: 16,
+  },
+  createAssignment: {
+    marginTop: 8,
+    alignContent: 'center',
+    backgroundColor: '#182f40',  
+    borderRadius: 5,
+  },
 
 })
 
 export default Assignment;
-
-{/* <DatePickerIOS
-              mode="date"
-              date={assignment.date}
-              onDateChange={ e => setDate(e)}
-            /> */}
