a6b3584d112dfa86b3d2bccd86c56beb1fa4a5c8
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-25 13:27:10 -0800

    Implement date picker on assignment modal

--- CheckerTexter/src/Assignment.js
+++ CheckerTexter/src/Assignment.js
@@ -16,12 +16,20 @@ function Assignment ({setModalInvisible, setTitle, setDate, visible, assignment}
 
             <Text>Enter information about your assignment.</Text>
             
-            <Text style={styles.font}>Title:</Text>
+            <Text style={[styles.font, styles.label]}>Title:</Text>
             <TextInput
               style={[styles.font, styles.titleInput]}
               placeholder="Assignment Title"
               onChange={ e => setTitle(e.nativeEvent.text) }
               value={assignment.title}
+            />
+
+            <Text style={[styles.font, styles.label]}>Date:</Text>
+            <DatePickerIOS
+              style={[styles.font, styles.dateInput]}
+              mode="date"
+              date={new Date()}
+              onDateChange={ e => setDate(e)}
             />
 
             <TouchableOpacity
@@ -45,13 +53,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#afbab5', 
+        backgroundColor: 'white', 
         alignContent:'stretch',
         // color: '#afbab5',
     },
     font: {
-        fontSize: 18,
-        letterSpacing: 1.5,
-        padding: 8,
+      fontSize: 18,
+      letterSpacing: 1.5,
+      padding: 8,
+    },
+    label: {
+      alignContent: 'flex-start',
     },
     select: {
         fontSize: 18,
@@ -65,6 +77,12 @@ const styles = StyleSheet.create({
       padding: 10,
       borderColor: 'gray',
       borderWidth: 1,
+      borderRadius: 2,
+      backgroundColor: 'white',
+      width: 350,
+      marginBottom: 16,
+    },
+    dateInput: {
       backgroundColor: 'white',
       width: 350,
       marginBottom: 16,
@@ -79,3 +97,9 @@ const styles = StyleSheet.create({
 })
 
 export default Assignment;
+
+{/* <DatePickerIOS
+              mode="date"
+              date={assignment.date}
+              onDateChange={ e => setDate(e)}
+            /> */}
